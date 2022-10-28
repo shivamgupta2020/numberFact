60e8108afbdf1af123b0b87306425574ba4ba12f
shivamgupta2020 <shivam.gupta@gmail.com>
2022-10-28 14:24:45 +0530

    updated api

--- src/App.js
+++ src/App.js
@@ -1,30 +1,52 @@
-import "./styles.css";
 import { useState } from "react";
+import "./App.css";
 
-export default function App() {
+const OPTIONS = [
+  { label: "trivia", value: "trivia" },
+  { label: "math", value: "math" },
+  { label: "date", value: "date" },
+  { label: "year", value: "year" },
+];
+
+function App() {
   const [number, setNumber] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(OPTIONS[0].value);
+  const [quote, setQuote] = useState("");
 
   const getQuote = () => {
-    fetch(`http://numbersapi.com/${number}`).
-    then((res) => {
-      console.log(res.json());
-    })
-    .catch((e)=>console.log(e))
-      
-    };
-  
+    fetch(`http://numbersapi.com/${number}/${selectedOption}`)
+      .then((response) => response.text())
+      .then((result) => {
+        setQuote(result);
+      });
+  };
 
   return (
     <div className="App">
       <input
         type="text"
+        id="number"
         value={number}
         onChange={(e) => {
           setNumber(e.target.value);
         }}
       />
-      <button onClick={getQuote}>Click Me</button>
+      <select
+        id="option"
+        value={selectedOption}
+        onChange={(e) => {
+          setSelectedOption(e.target.value);
+        }}
+      >
+        {OPTIONS.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
+      </select>
+
+      <button id="click" onClick={getQuote}>Click Me</button>
+      <div id="result">{quote}</div>
     </div>
   );
 }
 
+export default App;
